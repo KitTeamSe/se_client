120da69d1af37ebcaf7433c9e1b1c387865c5541
gilmujjang <alsanrlf@naver.com>
2021-07-28 17:17:07 +0900

    [REFACTORING] 라우터를 page 기반으로 정리

---
+++ src/pages/ProfilePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoginDialogContainer from '../containers/LoginDialog/LoginDialogContainer';
+import ProfilePageContainer from '../containers/ProfilePage/ProfilePageContainer';
+
+const ProfilePage = () => (
+  <>
+    <LoginDialogContainer />
+    <ProfilePageContainer />
+  </>
+);
+export default ProfilePage;
