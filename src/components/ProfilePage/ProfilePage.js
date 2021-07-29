5f139c96f45f782c1a028e468704162a5a1425d7
gilmujjang <alsanrlf@naver.com>
2021-07-29 22:22:10 +0900

    [ADD] 프로필 페이지 styled component 설정

--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -3,31 +3,49 @@ import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import styled from 'styled-components';
+
+const InfoTableWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const InfoTable = styled(TableContainer)`
+  margin-top: 24px;
+  max-width: 540px;
+`;
+
+const Welcome = styled.div`
+  margin: 24px;
+  font-size: 24px;
+`;
 
 const PropfilePage = props => {
   const { info } = props;
   const rows = Object.entries(info);
 
   return (
-    <TableContainer>
-      <Table>
-        <TableHead>
-          <div>기본정보</div>
-        </TableHead>
-        <TableBody>
-          {rows.map(row => (
-            <TableRow key={row[0]}>
-              <TableCell component="th" scope="row">
-                {row[0]}
-              </TableCell>
-              <TableCell align="right">{row[1]}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <InfoTableWrapper>
+      <Welcome>기본정보</Welcome>
+      <InfoTable>
+        <Table>
+          <TableBody>
+            {rows.map(row => (
+              <TableRow key={row[0]}>
+                <TableCell component="th" scope="row">
+                  {row[0]}
+                </TableCell>
+                <TableCell align="right">{row[1]}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </InfoTable>
+    </InfoTableWrapper>
   );
 };
 
