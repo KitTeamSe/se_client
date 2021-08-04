cdc50c2dcbad35a2bccc041d72d82f1e1e969f03
gilmujjang <alsanrlf@naver.com>
2021-08-04 19:09:12 +0900

    [ADD] 로그인 화면 디자인

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import LoginDialogContainer from '../containers/LoginDialog/LoginDialogContainer';
 import SignupPageContainer from '../containers/SignupPage/SignupPageContainer';
 
 const SignupPage = () => (
   <>
+    <LoginDialogContainer />
     <SignupPageContainer />
   </>
 );
