81f67dd50d61a38acdac6037bf7b053a0d9b84e6
hwanfront <kim015jh@gmail.com>
2021-09-25 23:47:12 +0900

    [ADD] 토큰만료 시 토큰삭제 및 화면 새로고침
    - client에서 interceptor를 사용해 전체적으로 작동
    - reply, tag에 각각 적혀있던 토큰만료 삭제는 없앰
    - 코드 GE03 권한없음으로는 잘 작동했는데 GE05는 만료토큰 기다리는게
      오래걸려서 한번만 작동해봤는데 혹시나 기회나면 테스트해보시길...

--- src/libs/api/tag.js
+++ src/libs/api/tag.js
@@ -6,10 +6,6 @@ export const searchTag = ({ text }) => {
   const token = localStorage.getItem('token');
 
   return client.get(`${URL}/match/${text}`, tokenHeader(token)).catch(error => {
-    if (error.response.data.code === 'GE05') {
-      localStorage.clear();
-      window.location.reload(true);
-    }
     throw error.response.data;
   });
 };
