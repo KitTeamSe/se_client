aa0a7c80adaa72fa0237431bff278dcafacebd01
hwanfront <kim015jh@gmail.com>
2021-11-21 23:22:26 +0900

    [ADD] 프로필 관련 반응형 처리
    - 프로필 페이지 mainwrapper 추가
    - ProfileHeader display 수정
    - 개인정보 수정 페이지 버튼 수정

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ProfilePageContainer from '../containers/ProfilePage/ProfilePageContainer';
+import { MainWrapper } from '../components/Common/Wrapper/Wrapper';
 
 const ProfilePage = () => {
-  return <ProfilePageContainer />;
+  return (
+    <MainWrapper>
+      <ProfilePageContainer />
+    </MainWrapper>
+  );
 };
 
 export default ProfilePage;
