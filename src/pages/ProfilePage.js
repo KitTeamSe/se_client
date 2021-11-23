5c4e0daae60e46034d9cc107846ba49faaeb558c
hwanfront <kim015jh@gmail.com>
2021-11-23 20:54:50 +0900

    [UPDATE] 프로필 width 수정

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 import ProfilePageContainer from '../containers/ProfilePage/ProfilePageContainer';
 import { MainWrapper } from '../components/Common/Wrapper/Wrapper';
 
+const ProfilePageWrapper = styled(MainWrapper)`
+  width: ${({ theme }) => theme.size.mobile};
+  @media ${({ theme }) => theme.sizeQuery.tablet} {
+    width: 600px;
+  }
+  @media ${({ theme }) => theme.sizeQuery.mobile} {
+    width: 100%;
+  }
+`;
+
 const ProfilePage = () => {
   return (
-    <MainWrapper>
+    <ProfilePageWrapper>
       <ProfilePageContainer />
-    </MainWrapper>
+    </ProfilePageWrapper>
   );
 };
 
