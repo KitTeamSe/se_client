cd62278a57cd610273344db68a2274c5e4913b0e
gilmujjang <alsanrlf@naver.com>
2021-12-30 18:22:31 +0900

    Revert "[DEL] jest 삭제"
    This reverts commit a697d474ebf2ddfb52c45e90981ee6bccf33443d.

---
+++ src/__test__/ProfileData.js
@@ -0,0 +1,45 @@
+export const infoObj = {
+  accountId: 4,
+  email: 'test@test.com',
+  idString: 'test',
+  informationOpenAgree: 'DISAGREE',
+  lastSignInIp: '119.202.133.187',
+  name: '길무짱',
+  nickname: 'FE_test',
+  phoneNumber: '01011332211',
+  studentId: '20133355',
+  type: 'STUDENT'
+};
+
+export const infoEditObj = {
+  accountId: 4,
+  email: 'test@test.com',
+  idString: 'test',
+  informationOpenAgree: 'DISAGREE',
+  lastSignInIp: '119.202.133.187',
+  name: '길무짱',
+  nickname: 'FE_test',
+  phoneNumber: '01011332211',
+  studentId: '20133355',
+  type: 'STUDENT',
+  password: ''
+};
+
+export const data = {
+  code: 'SR01',
+  data: {
+    accountId: 4,
+    email: 'test@test.com',
+    idString: 'test',
+    informationOpenAgree: 'DISAGREE',
+    lastSignInIp: '119.202.133.187',
+    name: '길무짱',
+    nickname: 'FE_test',
+    password: '',
+    phoneNumber: '01011332211',
+    studentId: '20133355',
+    type: 'STUDENT'
+  },
+  message: '성공적으로 조회되었습니다.',
+  status: 200
+};
