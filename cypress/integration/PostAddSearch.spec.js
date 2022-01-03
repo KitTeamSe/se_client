c786498797d65dfd0b7b49c82a8261eaeb6d628e
gilmujjang <alsanrlf@naver.com>
2022-01-03 18:16:32 +0900

    [TEST] 익명사용자 비밀글 작성, 삭제 테스트 코드 작성

--- cypress/integration/PostAddSearch.spec.js
+++ cypress/integration/PostAddSearch.spec.js
@@ -47,11 +47,81 @@ it('Post add anonymous user', () => {
   cy.contains(randomTitle).should('not.exist');
 });
 
-it('Normal Poting Logedin user', () => {
+it('Normal Post anonymous user', () => {
   cy.visit('/');
   const id = 'test';
   const pw = 'asdfasdf';
-  const nick = 'FE_test';
+  const randomTitle = `${String(Math.random()).substring(
+    2,
+    6
+  )}_Test Post Add Anonymous User`;
+  const randomText = String(Math.random()).substring(2, 8);
+
+  // 익명 게시글 작성
+  cy.contains('글쓰기').click();
+  cy.get('#title').type(`${randomTitle}`);
+  cy.get('.ck-content > p').type(randomText, { delay: 10 });
+  cy.get('#anonymousNickname').type(id, { delay: 10 });
+  cy.get('#anonymousPassword').type(pw, { delay: 10 });
+  cy.contains('작성').click();
+  cy.visit('/');
+
+  // 게시글 삭제
+  cy.contains(randomTitle).click();
+  cy.get('#more').click();
+  cy.get('#anonyDelete').click();
+  cy.contains('게시글 삭제').should('be.visible');
+  cy.get('#nowPassword').type(pw, { delay: 10 });
+  cy.get('#delBtn').click();
+  cy.contains('성공적으로 삭제되었습니다').should('be.visible');
+  cy.visit('/');
+  cy.contains(randomTitle).should('not.exist');
+});
+
+it('Secret Posting add anonymous user', () => {
+  cy.visit('/');
+  const id = 'test';
+  const pw = 'asdfasdf';
+  const randomTitle = `${String(Math.random()).substring(
+    2,
+    6
+  )}_Test Post Add Anonymous User`;
+  const randomText = String(Math.random()).substring(2, 8);
+
+  // 익명 게시글 작성
+  cy.contains('글쓰기').click();
+  cy.get('#title').type(`${randomTitle}`);
+  cy.get('.ck-content > p').type(randomText, { delay: 10 });
+  cy.get('#anonymousNickname').type(id, { delay: 10 });
+  cy.get('#anonymousPassword').type(pw, { delay: 10 });
+  cy.get('#isSecret').check();
+  cy.contains('작성').click();
+  cy.visit('/');
+
+  // 여러조건으로 검색
+  Search('TITLE', randomTitle, randomTitle);
+  Search('TITLE_TEXT', randomTitle, randomTitle);
+  Search('NICKNAME', id, randomTitle);
+
+  // 게시글 삭제
+  cy.contains(randomTitle).find('svg').should('be.visible');
+  cy.contains(randomTitle).click();
+  cy.get('#nowPassword').type(pw, { delay: 10 });
+  cy.contains('확인').click();
+  cy.get('#more').click();
+  cy.get('#anonyDelete').click();
+  cy.contains('게시글 삭제').should('be.visible');
+  cy.get('#nowPassword').type(pw, { delay: 10 });
+  cy.get('#delBtn').click();
+  cy.contains('성공적으로 삭제되었습니다').should('be.visible');
+  cy.visit('/');
+  cy.contains(randomTitle).should('not.exist');
+});
+
+it('Normal Posting Logedin user', () => {
+  cy.visit('/');
+  const id = 'test';
+  const pw = 'asdfasdf';
   const tag = '1학년';
   const randomTitle = `${String(Math.random()).substring(
     2,
@@ -85,18 +155,13 @@ it('Normal Poting Logedin user', () => {
 
   // 댓글 작성
   cy.contains(randomTitle).should('be.visible').click();
-  cy.get('.ck-content > p').type(randomReply, { delay: 10 });
+  cy.get('.ck-placeholder').type(randomReply, { delay: 10 });
   cy.contains('작성').click();
   cy.visit('/');
 
   // 여러 조건으로 검색
-  Search('TITLE_TEXT', randomText, randomTitle);
-  Search('TITLE_TEXT', randomTitle, randomTitle);
-  Search('TITLE', randomTitle, randomTitle);
-  Search('TEXT', randomText, randomTitle);
+
   Search('REPLY', randomReply, randomTitle);
-  Search('NICKNAME', nick, randomTitle);
-  Search('USERID', id, randomTitle);
   Search('TAG', tag, randomTitle);
 
   // 게시글 삭제
